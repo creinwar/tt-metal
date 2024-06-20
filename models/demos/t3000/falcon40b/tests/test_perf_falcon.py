1e47b38cb8432c8a052df36fdd9394e54b6c6c4b
Johanna Rock <jrock@tenstorrent.com>
2024-06-20 08:37:11 +0000

    #8049: Add fused layernorm to falcon40b

--- models/demos/t3000/falcon40b/tests/test_perf_falcon.py
+++ models/demos/t3000/falcon40b/tests/test_perf_falcon.py
@@ -249,6 +249,8 @@ def run_test_FalconCausalLM_end_to_end(
         )
 
     # Run for perf iteration - profiler enabled
+    for device in devices:
+        tt_lib.device.DumpDeviceProfiler(device)
     profiler.enable()
     enable_persistent_kernel_cache()
     logger.info(f"Enable profiler and enable binary and compile cache")
@@ -335,6 +337,7 @@ def run_test_FalconCausalLM_end_to_end(
         ("prefill", 1, 128, 0, 60, 0.46 + 0.04, 60, "BFLOAT16-DRAM"),
         ("prefill", 1, 2048, 0, 60, 1.18 + 0.1, 60, "BFLOAT16-DRAM"),
         ("decode", 32, 1, 128, 60, 0.21 + 0.02, 60, "BFLOAT8_B-SHARDED"),
+        ("prefill", 1, 2048, 0, 1, 0.94 + 0.1, 60, "BFLOAT8_B-DRAM"),
     ),
     ids=[
         "prefill_seq32_bfp8",
@@ -344,6 +347,7 @@ def run_test_FalconCausalLM_end_to_end(
         "prefill_seq128_fp16",
         "prefill_seq2048_fp16",
         "decode_batch32",
+        "prefill_seq2048_bfp8_layers_1",
     ],
 )
 @pytest.mark.parametrize(
