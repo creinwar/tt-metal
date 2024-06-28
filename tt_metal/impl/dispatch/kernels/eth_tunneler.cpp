778589036f402b978b8ea270331645d0da14f837
David Ma <davidma@tenstorrent.com>
2024-06-28 23:54:34 +0000

    #0: This works?

--- tt_metal/impl/dispatch/kernels/eth_tunneler.cpp
+++ tt_metal/impl/dispatch/kernels/eth_tunneler.cpp
@@ -155,7 +155,7 @@ void kernel_main() {
         tt_l1_ptr launch_msg_t * const launch_msg = GET_MAILBOX_ADDRESS_DEV(launch);
         if (launch_msg->exit_erisc_kernel) {
             DEBUG_STATUS("EEB");
-            break;
+            return;
         }
         // need to optimize this.
         // context switch to base fw is very costly.
