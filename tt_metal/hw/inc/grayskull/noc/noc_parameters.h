8545e316be4c4db4cc0a603bb7999dc81b7d449d
Almeet Bhullar <abhullar@tenstorrent.com>
2024-06-28 20:26:33 +0000

    #9620: Hack workaround needs to be fixed when passing pcie base addr to device

--- tt_metal/hw/inc/grayskull/noc/noc_parameters.h
+++ tt_metal/hw/inc/grayskull/noc/noc_parameters.h
@@ -9,10 +9,15 @@
 #define PCIE_NOC_X 0
 #define PCIE_NOC_Y 4
 
+#define PCIE_NOC1_X
+#define PCIE_NOC1_Y
+
 // Address formats
 #define NOC_XY_ENCODING(x, y) ((((uint32_t)(y)) << (NOC_ADDR_NODE_ID_BITS)) | (((uint32_t)(x))))
 
-#define NOC_XY_PCIE_ENCODING(x, y, noc_index) NOC_XY_ENCODING(x, y)
+#define NOC_XY_PCIE_ENCODING(x, y, noc_index)
+    NOC_XY_ENCODING(x, y) | \
+    ((noc_index ? (x == PCIE_NOC1_X and y == PCIE_NOC1_Y) : (x == PCIE_NOC_X and y == PCIE_NOC_Y)) * NOC_PCIE_ADDR_BASE) \
 
 #define NOC_MULTICAST_ENCODING(x_start, y_start, x_end, y_end)                                          \
     ((x_start) << (2 * NOC_ADDR_NODE_ID_BITS)) | ((y_start) << (3 * NOC_ADDR_NODE_ID_BITS)) | (x_end) | \
