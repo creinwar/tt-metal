75b851f467a8749c6d14686c5dc3bb6063e701fc
--global <--global>
2024-07-02 23:23:15 +0000

    #0: Add additional eth ubench (link roudtrip latency)

--- tests/tt_metal/microbenchmarks/ethernet/test_ethernet_link_ping_latency.py
+++ tests/tt_metal/microbenchmarks/ethernet/test_ethernet_link_ping_latency.py
@@ -8,11 +8,7 @@ import sys
 from loguru import logger
 import pytest
 from tt_metal.tools.profiler.process_device_log import import_log_run_stats
-import tt_metal.tools.profiler.device_post_proc_config as device_post_proc_config
-
-
-def fits_in_l1(num_channels, sample_size):
-    return num_channels * sample_size <= 150 * 1024
+import tt_metal.tools.profiler.device_post_proc_config as device_possetupt_proc_config
 
 
 @pytest.mark.skip("#7754: Failing with rc != 0 in main")
@@ -23,15 +19,17 @@ def fits_in_l1(num_channels, sample_size):
         (
             512,
             1024,
-            2048,
-            4096,
-            16384,
         )
     ],
 )  # , 1024, 2048, 4096],
 @pytest.mark.parametrize(
     "channel_counts",
-    [(1, 2, 4, 8, 16)],
+    [
+        (
+            1,
+            2,
+        )
+    ],
 )  # , 2, 3, 4])
 def test_bidirectional_erisc_bandwidth(sample_counts, sample_sizes, channel_counts):
     test_string_name = f"test_ethernet_send_data_microbenchmark - \
@@ -41,16 +39,13 @@ def test_bidirectional_erisc_bandwidth(sample_counts, sample_sizes, channel_coun
     print(f"{test_string_name}")
     os.system(f"rm -rf {os.environ['TT_METAL_HOME']}/generated/profiler/.logs/profile_log_device.csv")
 
-    if not fits_in_l1(channel_counts[0], sample_sizes[0]):
-        pytest.skip("Data does not fit in L1. Skipping")
-
     sample_counts_str = " ".join([str(s) for s in sample_counts])
     sample_sizes_str = " ".join([str(s) for s in sample_sizes])
     channel_counts_str = " ".join([str(s) for s in channel_counts])
 
     rc = os.system(
         f"TT_METAL_DEVICE_PROFILER=1 \
-            {os.environ['TT_METAL_HOME']}/build/test/tt_metal/perf_microbenchmark/ethernet/test_ethernet_bidirectional_bandwidth_no_edm \
+            {os.environ['TT_METAL_HOME']}/build/test/tt_metal/perf_microbenchmark/ethernet/test_ethernet_link_ping_latency_no_edm \
                 {len(sample_counts)} {sample_counts_str} \
                     {len(sample_sizes)} {sample_sizes_str} \
                         {len(channel_counts)} {channel_counts_str} \
